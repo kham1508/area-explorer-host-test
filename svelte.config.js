88a226813875ba5fb3e4b6892ce712883a3cc254
kham1508 <kyle.hamilton@nisra.gov.uk>
2022-06-16 16:22:27 +0100

    add /docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,23 @@
+/** @type {import("@sveltejs/kit").Config} */
 import adapter from "@sveltejs/adapter-static";
 // was "@sveltejs/adapter-auto"
 
 const dev = "production" === "development";
 
-/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: "docs",
-			assets: "docs"
-		}),
-		paths: {
-			// change below to your repo name
-			base: dev ? "" : "/area-explorer-host-test",
+		kit: {
+				// hydrate the <div id="svelte"> element in src/app.html
+				// target: "#svelte",
+				adapter: adapter({
+						pages: "docs",
+						assets: "docs"
+				}),
+				paths: {
+						// change below to your repo name
+						base: dev ? "/area-explorer-host-test" : ""
+				}
+
 		}
-		// ,
-		// hydrate the <div id="svelte"> element in src/app.html
-		// target: "#svelte"
-	}
 };
 
 export default config;
